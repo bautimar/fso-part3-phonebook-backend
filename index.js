6908f8f5a70fe0a10575f1b28ad8a02cd63897ae
Mark Philip Bautista <49254424+bautimar@users.noreply.github.com>
2024-06-07 18:46:28 -0700

    Finish part3a exercises 3.5 and 3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -52,6 +54,38 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000) + 1;
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name is missing'
+        })
+    } else if (!body.number) {
+        return response.status(400).json({
+            error: 'number is missing'
+        })
+    } else if (persons.find(person => person.name === body.name) !== undefined) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    } 
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
