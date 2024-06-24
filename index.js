632b9609c75aa687432b6213e814dad0f44c9a8e
Mark Philip Bautista <49254424+bautimar@users.noreply.github.com>
2024-06-24 16:40:16 -0700

    Finish part3b exercise 3.11 (full stack phonebook)

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('post', function getPost(request) {
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
 let persons = [
